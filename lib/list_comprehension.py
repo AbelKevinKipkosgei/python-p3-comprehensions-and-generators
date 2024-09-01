caa14e604b5905d1ae013db7a2d528e30f0f36d6
AbelKevinKipkosgei <abelkevinkipkosgei@gmail.com>
2024-09-01 18:21:14 +0300

    Learned list comprehensions and generators in Python.

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,15 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    even_list = [num for num in num_list if num % 2 == 0]
+    print(even_list)
+    return even_list
+
 
 def make_exclamation(sentence_list):
-    pass
+    exclamation_list = [f"{string}!" for string in sentence_list]
+    print(exclamation_list)
+    return exclamation_list
+
+return_evens([0, 1, 2, 5, 7, 9, 10])
+make_exclamation(["Hello", "I'm doing great", "Python is fun"])
